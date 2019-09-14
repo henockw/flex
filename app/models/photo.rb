136839b4394ee6f0056e26007d408a5eca9afdd2
Henock Woldu <henockw@gmail.com>
2019-09-14 16:58:51 -0700

    Change the Resolution

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,4 @@
 class Photo < ApplicationRecord
-  belongs_to :place
+  belongs_to :course
   mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_09_12_193118) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["user_id"], name: "index_courses_on_user_id"
   end
 
@@ -57,7 +58,6 @@ ActiveRecord::Schema.define(version: 2019_09_12_193118) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
